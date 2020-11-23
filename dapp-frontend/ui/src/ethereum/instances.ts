bccfe9fc852e578f87f65c4a219f3448704f6061
Mridul Dey <mridul.dgp2015@gmail.com>
2020-11-23 23:15:06 +0530

    Integrate SurveyManager contract

--- dapp-frontend/ui/src/ethereum/instances.ts
+++ dapp-frontend/ui/src/ethereum/instances.ts
@@ -1,3 +1,5 @@
 import { ethers } from 'ethers';
-
+import { SurveyManagerFactory } from './typechain/SurveyManagerFactory';
 window.provider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/b81341e3ab894360a84f3fa640ab985e');
+
+window.surveyInstance = SurveyManagerFactory.connect('0x078bf531F1eF322CfedbdEFd74a67DC6E49a8A14', window.provider);
--- dapp-frontend/ui/src/global.ts
+++ dapp-frontend/ui/src/global.ts
@@ -1,11 +1,12 @@
 import { ethers } from 'ethers';
-
+import { SurveyManager } from "../src/ethereum/typechain/SurveyManager";
 // mark the typings of your global variables
 declare global {
   interface Window{
     provider: ethers.providers.JsonRpcProvider;
     wallet: any;
     ethereum: ethers.providers.ExternalProvider;
+    surveyInstance: SurveyManager;
     // wallet: CustomWallet | ethers.providers.JsonRpcSigner | undefined; // marking this as undefined helps to prevent many runtime bugs when wallet is not loaded
   }
 }
