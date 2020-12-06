4508fedf5d277152635ca3d88b389cfbab378190
Mridul Dey <mridul.dgp2015@gmail.com>
2020-12-06 23:40:31 +0530

    New contract address used

--- dapp-frontend/ui/src/ethereum/instances.ts
+++ dapp-frontend/ui/src/ethereum/instances.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
 import { SurveyManagerFactory } from './typechain/SurveyManagerFactory';
 window.provider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/91d5a5957f0e4b2193ff2eca17a43453');
-window.surveyInstance = SurveyManagerFactory.connect('0xe5389ba2b3de68A2CE987B0437896Cb1D585A23C', window.provider);
-// window.surveyInstance = SurveyManagerFactory.connect('0xd548Fe5e2d7fAae38ed891DeB4350501aD851e57', window.provider);    fresh contract
+// window.surveyInstance = SurveyManagerFactory.connect('0xe5389ba2b3de68A2CE987B0437896Cb1D585A23C', window.provider); old contract
+window.surveyInstance = SurveyManagerFactory.connect('0xd548Fe5e2d7fAae38ed891DeB4350501aD851e57', window.provider);
